1d98a8a8249843d28e8cee469da90147004d0e89
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-03-10 16:50:01 +0530

    DMA

--- Dyanamic Memory Allocation/DyanamicAllocation.cpp
+++ Dyanamic Memory Allocation/DyanamicAllocation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<math.h>
 using namespace std;
 
 // DMA:(help to create memory in 'Heap' rather than 'Stack')
@@ -9,4 +10,29 @@ int main(){
     int *p = new int;
     *p = 10;
     cout << *p <<endl;
+
+
+    // Array allocation
+    int *pa = new int[50];
+
+    //eg.
+    int n;
+    cout << "Enter the number of elemt want :";
+    cin >> n;
+    int *pa2 = new int[n];
+    for(int i =0; i<n;i++){
+        cin>>pa2[i];
+    }
+    
+    int max = -1;
+    for(int i =0;i<n;i++){
+        if(max < pa2[i]){
+            max = pa2[i];
+        }
+    }
+    cout << max <<endl;
+
+    delete pa2; //releaseing interger pointed by pa2(remember we are not deleting pa2 just deleting where its points.)
+    delete pa;
+    delete p;
 }
