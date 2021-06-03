920a034e8f110fae84900292416d95a052811e73
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-06-03 11:35:57 +0530

    rec

--- Recursion/NumberOfZeroes.cpp
+++ Recursion/NumberOfZeroes.cpp
@@ -1,11 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int zeroSum(int x)
+{
+    static int count = 0;
+    // cout << x <<" "<<endl;
+    // cout << count <<" "<<endl;
 
-int main() {
+    if (x > 0)
+    {
+
+        if (x % 10 == 0)
+
+            count++;
+
+        zeroSum(x / 10);
+    }
+    return count;
+}
+
+int main()
+{
 
     int n;
     cin >> n;
 
-    
+    cout << zeroSum(n) << endl;
 }
